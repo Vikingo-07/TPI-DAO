2a6dcf4b135652bc90965439a8126f2ae33fe07d
zStyx <zStyx@protonmail.com>
2023-11-15 07:59:24 -0300

    Junto prestamos y devoluciones

--- Model/test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-
-fecha_1 = datetime.datetime.strptime("2023-11-20", "%Y-%m-%d")
-print(fecha_1)
-fecha_2 = datetime.datetime.strptime("2023-12-25", "%Y-%m-%d")
-
-print((fecha_2 - fecha_1).days)
